8c45b37a7eee64a9fc5e0838a68afe14c30998e8
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2020-07-23 11:29:28 +0200

    Updated copy disclaimer

--- src/test/java/org/seniorglez/karumiCodeTest/TestEmailFormatChecker.java
+++ src/test/java/org/seniorglez/karumiCodeTest/TestEmailFormatChecker.java
@@ -1,14 +1,29 @@
+/* This file is part of KarumiCodeTest
+ *
+ *  Copyright (c) 2020 Diego Domínguez González
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ *  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ *  of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
 package org.seniorglez.karumiCodeTest;
-
 import org.junit.jupiter.api.Test;
 import org.seniorglez.karumiCodeTest.views.login.EmailFormatChecker;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class TestEmailFormatChecker implements EmailFormatChecker {
 
     @Test
-    public void shouldReturnTrue() {
+    public void testIfTheEmailsAreValidShouldReturnTrue() {
         assertTrue(checkEmail("seniorglez@yahoo.com"));
         assertTrue(checkEmail("seniorglez@karumi.com"));
         assertTrue(checkEmail("seniorglez@karumi.com.es"));
@@ -16,7 +31,7 @@ public class TestEmailFormatChecker implements EmailFormatChecker {
     }
 
     @Test
-    public void shouldReturnFalse(){
+    public void testIfTheEmailsAreValidShouldReturnFalse() {
         assertTrue(!checkEmail("seniOrglez@yahoo.com"));
         assertTrue(!checkEmail("pepe*asda@yahoo.com"));
         assertTrue(!checkEmail("senior@yahoo.com.e"));
